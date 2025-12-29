7306193f8bbd797165ba102cb9130e9aaf1e8032
User <nadilveenath524@gmail.com>
2025-12-29 16:10:20 +0530

    add new changes to the Add to Favourites and View details buttons

--- frontend/src/components/PropertyCard.jsx
+++ frontend/src/components/PropertyCard.jsx
@@ -3,25 +3,37 @@ import "../styles/PropertyCard.css";
 
 function PropertyCard({ property, onFavourite }) {
     return (
-        <div className="property-card">
-            <img src={property.images[0]} alt={property.shortDescription} />
+        <div className="property-card" draggable="true">
+            {/* Main property image */}
+            <img
+                src={property.images[0]}
+                alt={property.shortDescription}
+                className="property-image"
+            />
 
+            {/* Property information */}
             <div className="property-info">
                 <h3>£{property.price.toLocaleString()}</h3>
                 <p>{property.shortDescription}</p>
 
-                <button onClick={() => onFavourite(property)}>
-                    ❤️ Add to Favourites
+                {/* Add to Favourites button */}
+                <button
+                    className="fav-btn"
+                    onClick={() => onFavourite(property)}
+                >
+                    Add to Favourites
                 </button>
 
-
-                <a href="/property/1" className="details-btn">
+                {/* Link to Property Details page */}
+                <Link
+                    to={`/property/${property.id}`}
+                    className="details-btn"
+                >
                     View Details
-                </a>
+                </Link>
             </div>
         </div>
     );
 }
-
 
 export default PropertyCard;
