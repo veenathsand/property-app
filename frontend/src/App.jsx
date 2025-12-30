09e0305258765433bfe200a8abc903a4f4279c37
User <nadilveenath524@gmail.com>
2025-12-30 11:11:24 +0530

    Update the file for set up footer.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import SearchPage from "./pages/SearchPage";
 import PropertyPage from "./pages/PropertyPage";
+import Footer from "./components/Footer";
 
 function App() {
     return (
-        <Routes>
-            <Route path="/" element={<SearchPage />} />
-            <Route path="/property/:id" element={<PropertyPage />} />
-        </Routes>
+        <>
+            <Routes>
+                <Route path="/" element={<SearchPage />} />
+                <Route path="/property/:id" element={<PropertyPage />} />
+            </Routes>
+
+            {/* Footer appears on all pages */}
+            <Footer />
+        </>
     );
 }
 
