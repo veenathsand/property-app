023881d3d19bd35e3f08d94f52f5ee34914d83bf
User <nadilveenath524@gmail.com>
2025-12-30 15:06:46 +0530

    Update package.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/property-app/",
   test: {
     globals: true,
     environment: "happy-dom",
